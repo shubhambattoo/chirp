526013372fee3e32db1fd54515c236b1a3ffe16b
shubhambattoo <shubham.battoo@hotmail.com>
2023-05-01 14:30:04 +0530

    added profile page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,4 @@
-import type { User } from "@clerk/nextjs/dist/api";
+
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
@@ -9,16 +9,9 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 
-const filterUsersList = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
-
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per minute
 const ratelimit = new Ratelimit({
@@ -45,7 +38,7 @@ export const postRouter = createTRPCRouter({
         userId: posts.map((post) => post.authorId),
         limit: 100,
       })
-    ).map(filterUsersList);
+    ).map(filterUserForClient);
 
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
